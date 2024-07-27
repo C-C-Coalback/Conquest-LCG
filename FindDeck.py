cbb1a22f68058ffb22d6f13955bf91afdbcc8d9c
unknown <argarfield@protonmail.com>
2024-07-27 11:07:39 +0200

    Now returns deck contents

--- FindDeck.py
+++ FindDeck.py
@@ -1,23 +1,32 @@
 def find_deck():
     file_to_read = open("deck_storage.txt", "r")
     deck_name = input("Enter deck name: ")
-    deck_found = False
     file_text = file_to_read.read()
     pos = 0
     current_deck = ""
-    while not deck_found and pos < len(file_text):
+    while pos < len(file_text):
         while file_text[pos] != "#":
             current_deck += file_text[pos]
             pos += 1
         if current_deck == deck_name:
             print("Deck found")
-            deck_found = True
+            file_to_read.close()
+            return read_deck(pos)
         else:
             while file_text[pos] != "\n":
                 pos += 1
             pos += 1
+            current_deck = ""
     file_to_read.close()
-    if not deck_found:
-        print("Deck not found")
-        return ""
-    return current_deck
+    print("Deck not found")
+    return ""
+
+
+def read_deck(pos):
+    file_to_read = open("deck_storage.txt", "r")
+    deck_contents = ""
+    file_text = file_to_read.read()
+    while file_text[pos] != "\n":
+        deck_contents += file_text[pos]
+        pos += 1
+    return deck_contents
