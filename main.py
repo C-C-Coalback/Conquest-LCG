8050c3f0c5c2eca7ab99dccdcc47a8d74afc0056
unknown <argarfield@protonmail.com>
2024-07-29 07:01:23 +0200

    New file for the player class

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import OrksCardsInit
 import FindCard
 import DeckHandling
 import FindDeck
-import random
+import PlayerClass
 
 snotling = CardClasses.TokenCard("Snotling", "", "Runt.", "Orks", 1, 1)
 
@@ -52,49 +52,7 @@ if holder == "y":
 
 # print_info_planet()
 
-
-class Player:
-    def __init__(self):
-        self.resources = 0
-        self.cards = []
-        self.victory_display = []
-        self.icons_gained = [0, 0, 0]
-        self.headquarters = []
-        self.deck = []
-
-    def get_resources(self):
-        return self.resources
-
-    def get_cards(self):
-        return self.cards
-
-    def get_victory_display(self):
-        return self.victory_display
-
-    def get_icons_gained(self):
-        return self.icons_gained
-
-    def get_headquarters(self):
-        return self.headquarters
-
-    def get_deck(self):
-        return self.deck
-
-    def add_card_to_deck(self, card_name):
-        self.deck.append(card_name)
-
-    def add_to_hq(self, card_name):
-        self.headquarters.append(card_name)
-
-    def print_deck(self):
-        for i in range(len(self.deck)):
-            print(self.deck[i])
-
-    def shuffle_deck(self):
-        random.shuffle(self.deck)
-
-
-player_one = Player()
+player_one = PlayerClass.Player()
 
 deck_string = FindDeck.find_deck()
 FindDeck.load_deck(deck_string, player_one)
