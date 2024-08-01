981a5b0b8d4be233a71e4db3628452cf9c40402b
unknown <argarfield@protonmail.com>
2024-08-01 14:40:30 +0200

    Play card function and array for storing cards in play have been moved to the player class.

--- PlayerClass.py
+++ PlayerClass.py
@@ -1,5 +1,6 @@
 import random
 import copy
+import FindCard
 
 
 class Player:
@@ -10,6 +11,7 @@ class Player:
         self.icons_gained = [0, 0, 0]
         self.headquarters = []
         self.deck = []
+        self.cards_in_play = [[] for _ in range(8)]
 
     def get_resources(self):
         return self.resources
@@ -28,6 +30,45 @@ class Player:
 
     def get_deck(self):
         return self.deck
+
+    def init_planets_in_game(self, planets_in_game):
+        for j in range(len(planets_in_game)):
+            self.cards_in_play[0].append(planets_in_game[j])
+            print(self.cards_in_play[0])
+
+    def print_cards_in_play(self):
+        print("No of planets:", len(self.cards_in_play[0]))
+        for i in range(len(self.cards_in_play[0])):
+            print("Cards at planet:", self.cards_in_play[0][i])
+            if not self.cards_in_play[i + 1]:
+                print("None")
+            for j in range(len(self.cards_in_play[i + 1])):
+                print(self.cards_in_play[i + 1][j].get_name())
+
+    def play_card(self, card_to_play, planet_id):
+        if FindCard.check_card_type(card_to_play, "Army"):
+            print(self.get_resources())
+            if self.spend_resources(card_to_play.get_cost()) == 0:
+                self.cards_in_play[planet_id].append(copy.deepcopy(card_to_play))
+                print("Played card")
+                return 0
+            else:
+                print("Insufficient resources")
+                return -1
+        elif FindCard.check_card_type(card_to_play, "Support"):
+            print(self.get_resources())
+            if self.spend_resources((card_to_play.get_cost())) == 0:
+                self.add_to_hq(card_to_play)
+                print("Played card to HQ")
+                for i in range(len(self.get_headquarters())):
+                    print(self.get_headquarters()[i].get_name())
+                return 0
+            else:
+                print("Insufficient resources")
+                return -1
+        else:
+            print("Not an army/support card")
+            return -1
 
     def add_card_to_deck(self, card_name):
         self.deck.append(card_name)
