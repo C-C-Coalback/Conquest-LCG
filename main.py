7af216f76dbd78999255bd1bfa3b4de81fd9ad34
C-C-Coalback <argarfield18@gmail.com>
2024-09-23 15:37:52 +0200

    Player two can participate in command struggles

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def deploy_phase(round_number, p_one, p_two):
 
 def resolve_command_struggle(planet_num, p_one, p_two):
     player_one_command = p_one.count_command_at_planet(planet_num)
-    player_two_command = 0
+    player_two_command = p_two.count_command_at_planet(planet_num)
     if player_one_command > player_two_command:
         print(player_one_command, "greater than",
               player_two_command, "at planet no", planet_num, "player one wins command")
@@ -136,6 +136,15 @@ def command_phase(round_number, p_one, p_two):
         p_one.move_warlord_to_planet(pos + 1)
     p_one.print_cards_in_play()
     p_one.print_headquarters()
+    planet_name = input("Choose a planet to send the Warlord to:")
+    pos = p_two.search_planets_in_game(planet_name)
+    if pos == -1:
+        print("Planet not found")
+    else:
+        print("Attempting to move Warlord")
+        p_two.move_warlord_to_planet(pos + 1)
+    p_two.print_cards_in_play()
+    p_two.print_headquarters()
     print("command:", round_number)
     planet_num = round_number
     planets_counted = 0
@@ -153,6 +162,16 @@ def command_phase(round_number, p_one, p_two):
                     c_res[0] += planet_array2[i].get_resources()
                     c_res[1] += planet_array2[i].get_cards()
                     print("test", c_res[0])
+        elif result == 2:
+            planet_name = p_two.get_planet_name_given_position(planet_num - 1)
+            print("Planet name:", planet_name)
+            for i in range(len(planet_array2)):
+                if planet_name == planet_array2[i].get_name():
+                    print("Resources of", planet_name, planet_array2[i].get_resources())
+                    print("Cards of", planet_name, planet_array2[i].get_cards())
+                    c_res[2] += planet_array2[i].get_resources()
+                    c_res[3] += planet_array2[i].get_cards()
+                    print("test", c_res[2])
         planets_counted += 1
         planet_num += 1
     print(c_res[0], c_res[1], c_res[2], c_res[3])
@@ -161,9 +180,16 @@ def command_phase(round_number, p_one, p_two):
     print("Player one gets", c_res[1], "cards from command struggle")
     for i in range(c_res[1]):
         p_one.draw_card()
+    print("Player two gets", c_res[2], "resources from command struggle")
+    p_two.add_resources(c_res[2])
+    print("Player two gets", c_res[3], "cards from command struggle")
+    for i in range(c_res[3]):
+        p_two.draw_card()
     # player two will need the same idea but with c_res[2] and c_res[3]
     print(p_one.get_resources())
     print(p_one.get_cards())
+    print(p_two.get_resources())
+    print(p_two.get_cards())
 
 
 def combat_phase(round_number, p_one, p_two):
