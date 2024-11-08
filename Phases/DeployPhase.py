af8218120d3a406589f47c62c0959531e04ddb72
C-C-Coalback <argarfield18@gmail.com>
2024-11-08 11:33:35 +0100

    Player one can deploy units and have them show up. Both players' units can be on screen at the same time

--- Phases/DeployPhase.py
+++ Phases/DeployPhase.py
@@ -69,7 +69,6 @@ def pygame_deploy_phase(round_number, p_one, p_two, game_screen):
                                                     object_holder = FindCard.find_card(p_two.get_cards()[position])
                                                     p_two.play_card(object_holder, position2 + 1)
                                                     draw_all(game_screen, p_one, p_two)
-
                 elif 594 < y < 686:
                     position = x
                     position = position - 300
@@ -81,6 +80,30 @@ def pygame_deploy_phase(round_number, p_one, p_two, game_screen):
                     else:
                         print("Player one hand selected, card index", position)
                         print("Number of cards in hand:", len(p_one.get_cards()))
+                        if len(p_one.get_cards()) > position:
+                            planet_chosen = False
+                            while not planet_chosen:
+                                for event2 in pygame.event.get():
+                                    if event2.type == pygame.QUIT:
+                                        pygame.quit()
+                                        sys.exit()
+                                    if event2.type == pygame.MOUSEBUTTONDOWN:
+                                        x2, y2 = pygame.mouse.get_pos()
+                                        if 319 < y2 < 376:
+                                            position2 = x2 - 60
+                                            remainder2 = position2 % 165
+                                            position2 = int(position2 / 165)
+                                            print(position2, remainder2)
+                                            if 84 < remainder2:
+                                                pass
+                                            else:
+                                                print("Planets selected, index", position2)
+                                                planet_chosen = True
+                                                if position < 7:
+                                                    object_holder = FindCard.find_card(p_one.get_cards()[position])
+                                                    p_one.play_card(object_holder, position2 + 1)
+                                                    draw_all(game_screen, p_one, p_two)
+
                 elif 319 < y < 376:
                     print("Planets selected")
 
