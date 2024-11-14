2022d6d871528d4501eea0f855f801cf3f4d51e3
C-C-Coalback <argarfield18@gmail.com>
2024-11-14 09:06:22 +0100

    Bug fix, cards now appear exhausted in HQ

--- PlayerClass.py
+++ PlayerClass.py
@@ -487,7 +487,7 @@ class Player:
                 if letter == " ":
                     card_image_name = card_image_name.replace(letter, "_")
             card_image = pygame.image.load(card_image_name).convert()
-            if not hq[i].get_ready:
+            if not hq[i].get_ready():
                 card_image = pygame.transform.rotate(card_image, 270)
             game_screen.blit(card_image, (x_c, y_c))
             x_c += increment
