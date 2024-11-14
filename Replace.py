11dde62af1393736163fffe8f704a33559938bdf
C-C-Coalback <argarfield18@gmail.com>
2024-11-14 10:30:29 +0100

    Resize damage token images

--- Replace.py
+++ Replace.py
@@ -7,13 +7,17 @@ def resize_files():
     path = dir_path + '/CardImages/'
     path2 = dir_path + '/ResizedImages/'
     planet_path = dir_path + '/PlanetImages/'
+    damage_tokens_path = dir_path + '/damagetokens/'
+    damage_tokens_400x400_path = dir_path + '/400x400damagetokens/'
     # input(path)
     # input(path2)
     dirs = os.listdir(path)
     image_list = []
     planet_image_list = []
     resized_images = []
+    resized_damage_images = []
     filenames = []
+    damage_token_names = []
     ratio = 0.17
 
     playmat_image = Image.open(dir_path + '/Playmat.png')
@@ -23,6 +27,19 @@ def resize_files():
     resources_image = Image.open(dir_path + '/Netrunner_credit.png')
     resources_image = resources_image.resize((50, 50))
     resources_image.save('{}{}'.format(dir_path + '/Netrunner_credit', '.png'))
+
+    for token in glob.glob(damage_tokens_400x400_path+'*.png'):
+        print(token)
+        base_name = os.path.basename(token)
+        name, ext = os.path.splitext(base_name)
+        damage_token_names.append(name)
+        img = Image.open(token)
+        img = img.resize((int(40), int(40)))
+        resized_damage_images.append(img)
+
+
+    for (a, new) in enumerate(resized_damage_images):
+        new.save('{}{}'.format(damage_tokens_path + damage_token_names[a], '.png'))
 
     for filename in glob.glob(path+'*.webp'):
         print(filename)
