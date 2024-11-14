44760ff0ba303d7314bf4f1be1eb10c21cd24902
C-C-Coalback <argarfield18@gmail.com>
2024-11-14 13:05:26 +0100

    Planets no longer appear in play after being captured

--- Phases/CombatPhase.py
+++ Phases/CombatPhase.py
@@ -242,9 +242,6 @@ def resolve_battle(p_one, p_two, planet_id, first_planet):
             p_two.capture_planet(planet_id)
     elif not player_one_check and not player_two_check:
         print("Neither player has units")
-    if first_planet:
-        p_one.toggle_planet_in_play(planet_id)
-        p_two.toggle_planet_in_play(planet_id)
 
 
 
@@ -269,6 +266,9 @@ def pygame_resolve_battle(p_one, p_two, planet_id, first_planet, game_screen):
             p_two.capture_planet(planet_id)
     elif not player_one_check and not player_two_check:
         print("Neither player has units")
+    if first_planet:
+        p_one.toggle_planet_in_play(planet_id)
+        p_two.toggle_planet_in_play(planet_id)
 
 
 def check_for_battle(p_one, p_two, planet_id, first_planet):
