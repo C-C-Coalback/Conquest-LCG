990cccaefec5e572dccdc8c8cdd84504d601cb2f
C-C-Coalback <argarfield18@gmail.com>
2024-11-26 10:41:53 +0100

    Player two commits warlord first if they have initiative.

--- Phases/CommandPhase.py
+++ Phases/CommandPhase.py
@@ -98,10 +98,16 @@ def pygame_commit_warlord_step(player):
 
 def pygame_command_phase(round_number, p_one, p_two, game_screen):
     planet_array2 = PlanetCardsInit.planet_cards_init()
-    pygame_commit_warlord_step(p_one)
-    draw_all(game_screen, p_one, p_two)
-    pygame_commit_warlord_step(p_two)
-    draw_all(game_screen, p_one, p_two)
+    if p_one.get_has_initiative():
+        pygame_commit_warlord_step(p_one)
+        draw_all(game_screen, p_one, p_two)
+        pygame_commit_warlord_step(p_two)
+        draw_all(game_screen, p_one, p_two)
+    else:
+        pygame_commit_warlord_step(p_two)
+        draw_all(game_screen, p_one, p_two)
+        pygame_commit_warlord_step(p_one)
+        draw_all(game_screen, p_one, p_two)
     print("command:", round_number)
     planet_num = round_number
     planets_counted = 0
