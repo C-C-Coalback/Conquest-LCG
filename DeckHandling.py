06f1a2c8029deabd8b45bbc41f776bc4e94c0e5a
C-C-Coalback <argarfield18@gmail.com>
2024-11-26 13:47:56 +0100

    User can now use the screen to create decks

--- DeckHandling.py
+++ DeckHandling.py
@@ -27,6 +27,49 @@ def pygame_create_deck(game_screen):
         warlord_card = FindCard.find_card(warlord_to_find)
     print("Card is a warlord!")
     deck_to_write = PromptText.prompt_text(game_screen, "Enter a name for the deck")
+    if warlord_card.get_name() == "Nazdreg":
+        deck_to_write += "#Nazdreg"
+        deck_to_write += "#Nazdreg's Flash Gitz"
+        deck_to_write += "#Nazdreg's Flash Gitz"
+        deck_to_write += "#Nazdreg's Flash Gitz"
+        deck_to_write += "#Nazdreg's Flash Gitz"
+        deck_to_write += "#Kraktoof Hall"
+        deck_to_write += "#Bigga is Betta"
+        deck_to_write += "#Bigga is Betta"
+        deck_to_write += "#Cybork Body"
+    required_faction = "Orks"
+    cards_added_array = []
+    deck_size = 8
+    while deck_size < 13:
+        card_to_add = PromptText.prompt_text(game_screen, "Enter card name to add")
+        card_object = FindCard.find_card(card_to_add)
+        if FindCard.check_faction(card_object, required_faction):
+            if FindCard.check_loyalty(card_object, "Signature"):
+                print("Card is a signature card, may not be added.")
+            else:
+                already_added = False
+                for cards_added in cards_added_array:
+                    if cards_added == card_object.get_name():
+                        already_added = True
+                if already_added:
+                    print("At least one copy of this card has already been added.")
+                else:
+                    copies_to_add = int(PromptText.prompt_text(game_screen, "Enter number of copies"))
+                    if copies_to_add > 3:
+                        print("Can only add a maximum of 3 copies of a card to a deck.")
+                    elif copies_to_add < 0:
+                        print("Invalid number entered.")
+                    elif copies_to_add == 0:
+                        print("Card not added.")
+                    else:
+                        print("Adding", copies_to_add, "copies of", card_object.get_name())
+                        while copies_to_add != 0:
+                            deck_to_write += "#"
+                            deck_to_write += card_object.get_name()
+                            deck_size = deck_size + 1
+                            copies_to_add = copies_to_add - 1
+                        cards_added_array.append(card_object.get_name())
+    write_deck_into_file(deck_to_write)
 
 def create_deck():
     warlord_to_find = input("Enter a Warlord: ")
