8076ebacb1b3876a43a0c9fad4a28d8e02f648b8
C-C-Coalback <argarfield18@gmail.com>
2024-11-29 21:37:53 +0100

    Added file to check if action button was pressed.

---
+++ ActionChecker.py
@@ -0,0 +1,11 @@
+def check_for_action(x, y, player_number):
+    print(x, y, player_number)
+    if player_number == 1:
+        if 1100 < x < 1200 and 650 < y < 700:
+            print("player one pass")
+            return 1
+    else:
+        if 0 < x < 100 and 50 < y < 100:
+            print("player two pass")
+            return 1
+    return 0
